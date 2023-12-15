30e8ce31311d752e845f76490f068910c711b53b
raghiahaidari <raghiahaidari02@gmail.com>
2023-12-15 18:05:02 +0100

    first page v1

--- src/main/java/com/example/javafx_foodrecommandation/HelloApplication.java
+++ src/main/java/com/example/javafx_foodrecommandation/HelloApplication.java
@@ -12,13 +12,13 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Hello!");
+        stage.setTitle("Meals");
         stage.setScene(scene);
         stage.show();
-
     }
 
     public static void main(String[] args) {
         launch();
     }
 }
+
