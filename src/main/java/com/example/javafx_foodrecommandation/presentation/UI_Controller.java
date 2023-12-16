91fdf5867ec3945a3f725d1b8d20c1790cdcc317
Roumaysae <roumaysaeelamrani2002@gmail.com>
2023-12-16 18:12:37 +0100

    personalize page v1

--- src/main/java/com/example/javafx_foodrecommandation/presentation/UI_Controller.java
+++ src/main/java/com/example/javafx_foodrecommandation/presentation/UI_Controller.java
@@ -2,34 +2,33 @@ package com.example.javafx_foodrecommandation.presentation;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.DragEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node; // Import Node from JavaFX
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
 public class UI_Controller {
-    @FXML
-    private Label welcomeText;
-
-        @FXML
-        private Button btn;
-
-        @FXML
-        private AnchorPane header;
-
-        @FXML
-        private AnchorPane imageFood;
-
-        @FXML
-        private AnchorPane mainTitle;
-
-        @FXML
-        private AnchorPane paneHome;
 
     @FXML
-    void submit(ActionEvent event) {
+    private void handleViewMealsClick(ActionEvent event) {
+        try {
+            // Load the personalize.fxml file
+            Parent personalizeParent = FXMLLoader.load(getClass().getResource("/com/example/javafx_foodrecommandation/personalize.fxml"));
+            Scene personalizeScene = new Scene(personalizeParent);
 
+            // Get the Stage information
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene
+            window.setScene(personalizeScene);
+            window.show();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception accordingly
+        }
     }
 
-    }
+    // Other methods and variables for your controller
+}
