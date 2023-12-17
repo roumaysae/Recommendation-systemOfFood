9823b1c83d0f081b2b01f38a7c9fd133275fa14e
Roumaysae <Roumaysaeelamrani2002@gmail.com>
2023-12-17 10:57:33 +0100

    THird version

---
+++ src/main/java/com/example/javafx_foodrecommandation/presentation/UI_Meal_ReadMore.java
@@ -0,0 +1,26 @@
+package com.example.javafx_foodrecommandation.presentation;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class UI_Meal_ReadMore {
+
+    @FXML
+    private VBox Meal_container;
+
+    @FXML
+    private Text decsription_card;
+
+    @FXML
+    private HBox igredientbox;
+
+    @FXML
+    private HBox preparationsteps;
+
+    @FXML
+    private Label title_card;
+
+}
