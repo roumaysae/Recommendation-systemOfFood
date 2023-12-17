9290289b99cd1395bfb6da45d97301493bab049f
Roumaysae <Roumaysaeelamrani2002@gmail.com>
2023-12-17 17:07:50 +0100

    nextVersion

--- src/main/java/com/example/javafx_foodrecommandation/presentation/UI_Consulting_Meals.java
+++ src/main/java/com/example/javafx_foodrecommandation/presentation/UI_Consulting_Meals.java
@@ -1,11 +1,38 @@
 package com.example.javafx_foodrecommandation.presentation;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class UI_Consulting_Meals {
 
     @FXML
     private BorderPane Container;
 
+    @FXML
+    private Text IngredientMeal;
+
+    @FXML
+    private Text InstructionsMeal;
+
+    @FXML
+    private VBox chosenmealCard;
+
+    @FXML
+    private GridPane gridpane;
+
+    @FXML
+    private HBox imageMeal;
+
+    @FXML
+    private HBox nameMeal;
+
+    @FXML
+    private ScrollPane scrollPan;
+
 }
+
