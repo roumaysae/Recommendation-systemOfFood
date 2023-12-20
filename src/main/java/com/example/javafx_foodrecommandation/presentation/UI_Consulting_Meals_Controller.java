deca747fe0b01c55ca982275813948588cbe33e0
Roumaysae <Roumaysaeelamrani2002@gmail.com>
2023-12-20 15:58:21 +0100

    Version 7.1

--- src/main/java/com/example/javafx_foodrecommandation/presentation/UI_Consulting_Meals_Controller.java
+++ src/main/java/com/example/javafx_foodrecommandation/presentation/UI_Consulting_Meals_Controller.java
@@ -1,13 +1,18 @@
 package com.example.javafx_foodrecommandation.presentation;
 
 import com.example.javafx_foodrecommandation.dao.Meal;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -40,12 +45,10 @@ public class UI_Consulting_Meals_Controller implements Initializable {
     private List<Meal> getData() {
         List<Meal> meals = new ArrayList<>();
         Meal meal;
-        for (int i = 0; i < meals.size(); i++) {
+        for (int i = 0; i <meals.size(); i++) {
             meal = new Meal();
             meal.setTitle("Lamb Pilaf ");
             meal.setImageMeal("../../resources/com/example/javafx_foodrecommandation/public/meal1Consulting.jpg");
-            meal.setInstructions("50g Lamb120g Prunes1 tbs Lemon Juice2 tbs Butter1 chopped Onion450g Lamb2 cloves Garlic600ml Vegetable Stock2 cups RicePinch SaffronGarnish Parsley");
-            meal.setCountry("Russia");
             meals.add(meal);
         }
         return meals;
@@ -60,20 +63,28 @@ public class UI_Consulting_Meals_Controller implements Initializable {
             for (int i = 0; i < mealList.size(); i++) {
                 FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setLocation(getClass().getResource("/resources/com/example/javafx_foodrecommandation/UI_Card_Meals.fxml"));
-                AnchorPane anchorPane = fxmlLoader.load();
+
                 UI_Card_Meals_Controller ui_card_meals = fxmlLoader.getController();
-                                ui_card_meals.setData(mealList.get(i));
+                ui_card_meals.setData(mealList.get(i));
+
+                AnchorPane anchorPane = fxmlLoader.load();
+
                     if(column==3){
                         column=0;
                         row++;
                     }
                 gridpane.add(anchorPane,column++,row);//child + col + row
-                gridpane.setMargin(anchorPane,new Insets(10));
+                GridPane.setMargin(anchorPane,new Insets(10));
             }
-
         } catch(IOException e){
             throw new RuntimeException(e);
         }
         }
-    }
+
+    @FXML
+    private void controllerMealsCategory(ActionEvent event) {
+
+
+        }
+}
 
