31eaf9cb53486ba5c76aa416631eebc73a661ef1
raghia <raghiahaidari02@gmail.com>
2024-01-12 15:20:46 +0100

    v13

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,5 +9,6 @@ module com.example.javafx_foodrecommandation {
     opens com.example.javafx_foodrecommandation to javafx.fxml;
     exports com.example.javafx_foodrecommandation;
     exports com.example.javafx_foodrecommandation.presentation;
+    exports com.example.javafx_foodrecommandation.dao ;
     opens com.example.javafx_foodrecommandation.presentation to javafx.fxml;
 }
